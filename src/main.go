1e876a1cd8480508c24993f540ca6e5d72f45703
John J Lincoln <jlincoln88@gmail.com>
2019-02-12 21:44:35 -0500

    io and os imports as well as panic error helper function

--- src/main.go
+++ src/main.go
@@ -1,17 +1,27 @@
 package main
- 
+
 import (
     "fmt"
     "html"
     "log"
     "net/http"
+    "bufio"
+    "io"
+    "io/ioutil"
+    "os"
 )
- 
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
- 
+
     log.Fatal(http.ListenAndServe(":8080", nil))
- 
+
 }
