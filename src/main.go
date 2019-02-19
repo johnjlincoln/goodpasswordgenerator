ed2351e35c6106137dbbc172274c712d89941a45
Jay <jlincoln88@gmail.com>
2019-02-18 23:11:38 -0500

    all the json

--- src/main.go
+++ src/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
+
+// PasswordJSON is a json object containing a password string as well as the word count of the dictionary used
+// https://golangcode.com/json-encode-an-array-of-objects/ try this cleaner implementation
+type PasswordJSON struct {
+	Password            string `json:"password"`
+	DictionaryWordCount int    `json:"dictionaryWordCount"`
+}
 
 func check(e error) {
 	if e != nil {
@@ -49,21 +58,31 @@ func getDictionaryWordCount(words []string) int {
 func main() {
 	words, err := readWordDictionary("../slurp/1984-list.txt")
 	check(err)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintln(w, words)
-		check(err)
-	})
+	var specialChars = make([]string, 10)
+	specialChars[0] = "*"
+	specialChars[1] = "!"
+	specialChars[2] = "@"
+	specialChars[3] = "#"
+	specialChars[4] = "$"
+	specialChars[5] = "%"
+	specialChars[6] = "~"
+	specialChars[7] = "_"
+	specialChars[8] = "?"
+	specialChars[9] = "+"
 
 	http.HandleFunc("/get/password", func(w http.ResponseWriter, r *http.Request) {
 		securePassowrd, dictionaryWordCount := getSecurePassword(words)
-		_, err := fmt.Fprintln(w, securePassowrd, dictionaryWordCount)
-		check(err)
+		securePasswordString := strings.Join(securePassowrd, "")
+		response := PasswordJSON{Password: securePasswordString, DictionaryWordCount: dictionaryWordCount}
+		fmt.Println("hit /get/password")
+		json.NewEncoder(w).Encode(response)
 	})
 
 	http.HandleFunc("/get/dictionary/length", func(w http.ResponseWriter, r *http.Request) {
-		dictionaryLength := getDictionaryWordCount(words)
-		_, err := fmt.Fprintln(w, dictionaryLength)
-		check(err)
+		dictionaryWordCount := getDictionaryWordCount(words)
+		response := PasswordJSON{DictionaryWordCount: dictionaryWordCount}
+		fmt.Println("hit /get/dictionary/length")
+		json.NewEncoder(w).Encode(response)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
