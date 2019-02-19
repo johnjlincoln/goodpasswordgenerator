9d01cda06224815bd1d6aa3a269264289b77d87a
Jay <jlincoln88@gmail.com>
2019-02-18 21:38:11 -0500

    convert spaces to tabs, get ultra-basic functionality working

--- src/main.go
+++ src/main.go
@@ -1,41 +1,70 @@
 package main
 
 import (
-    "fmt"
-    // "html"
-    "log"
-    "net/http"
-    "bufio"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func readWordDictionary(path string) ([]string, error) {
-    file, err := os.Open(path)
-    check(err)
-    defer file.Close()
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
 
-    var words []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        words = append(words, scanner.Text())
-    }
-    return words, scanner.Err()
+	var words []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	return words, scanner.Err()
+}
+
+func getSecurePassword(words []string) ([]string, int) {
+	var securePassowrd []string
+	dictionaryWordCount := len(words)
+	s := rand.NewSource(time.Now().Unix())
+	r := rand.New(s)
+	for len(securePassowrd) < 4 {
+		securePassowrd = append(securePassowrd, words[r.Intn(dictionaryWordCount)])
+	}
+	return securePassowrd, dictionaryWordCount
+}
+
+func getSecurePasswordOpts(words []string) {}
+
+func getDictionaryWordCount(words []string) int {
+	return len(words)
 }
 
 func main() {
-    words, err := readWordDictionary("../slurp/1984-list.txt")
-    check(err)
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        _, err := fmt.Fprintln(w, words)
-        check(err)
-    })
+	words, err := readWordDictionary("../slurp/1984-list.txt")
+	check(err)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintln(w, words)
+		check(err)
+	})
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/get/password", func(w http.ResponseWriter, r *http.Request) {
+		securePassowrd, dictionaryWordCount := getSecurePassword(words)
+		_, err := fmt.Fprintln(w, securePassowrd, dictionaryWordCount)
+		check(err)
+	})
 
+	http.HandleFunc("/get/dictionary/length", func(w http.ResponseWriter, r *http.Request) {
+		dictionaryLength := getDictionaryWordCount(words)
+		_, err := fmt.Fprintln(w, dictionaryLength)
+		check(err)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
